82959ac8f84dcf11db46b2fc4070b1a57172609c
Kristen Dybala <kdybala@gmail.com>
2021-09-24 13:24:14 -0700

    specify individual tidyverse packages

--- R/packages.R
+++ R/packages.R
@@ -1,5 +1,14 @@
-library(tidyverse)
+# library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(readr)
 library(sf)
 # library(raster)
 library(terra)
 library(DeltaMultipleBenefits)
+library(ggplot2)
+
+# remotes::install_local('C:/Users/kdybala/Documents/DeltaMultipleBenefits/')
+# recommended:
+# library(htmlwidgets)
+# library(leaflet)
