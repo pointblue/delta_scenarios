f915ed4ee9422d53a633297609c3a59d2630d207
Kristen Dybala <kdybala@gmail.com>
2021-09-27 17:11:50 -0700

    remove unneeded script

--- R/manage_webpages.R
+++
@@ -1,8 +0,0 @@
-
-index = rmarkdown::render(input = 'Rmd/index.Rmd',
-                          output_file = 'index.html',
-                          output_dir = 'docs')
-
-scenarios = rmarkdown::render(input = 'Rmd/draft_scenarios.Rmd',
-                          output_file = 'draft_scenarios.html',
-                          output_dir = 'docs')
