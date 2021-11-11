b91644044b920e08c766c753d27c2c4dc2c0c838
Kristen Dybala <kdybala@gmail.com>
2021-11-11 14:05:01 -0800

    clean up, and update to work with more detailed baseline layer

--- R/scenario_map.R
+++ R/scenario_map.R
@@ -4,40 +4,102 @@
 # PACKAGES & FUNCTIONS
 source('R/packages.R')
 
+delta_poly = read_sf('GIS/boundaries/Legal_Delta_Boundary.shp') %>%
+  st_transform(crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+
+key = readxl::read_excel('GIS/VEG_Delta10k_baseline_metadata.xlsx')
+
+key_simple <- key %>%
+  select(CODE_BASELINE, CLASS, SUBCLASS, DETAIL, CODE_NAME, COLOR) %>%
+  filter(CODE_NAME %in%
+           c('ORCHARD_DECIDUOUS', 'VINEYARD', 'GRAIN&HAY', 'FIELD',
+             'FIELD_CORN', 'RICE', 'IDLE', 'PASTURE', 'PASTURE_ALFALFA',
+             'GRASSLAND', 'URBAN', 'RIPARIAN', 'WETLAND_MANAGED',
+             'WETLAND_TIDAL', 'WETLAND_OTHER', 'WATER', 'WOODLAND&SCRUB',
+             'BARREN')) %>%
+  mutate(label = if_else(!is.na(CLASS), CLASS,
+                         if_else(!is.na(SUBCLASS), SUBCLASS, DETAIL)),
+         label = capwords(tolower(label)),
+         label = recode(label, 'Deciduous Fruits & Nuts' = 'Orchard',
+                        'Grain & Hay' = 'Grains',
+                        'Field Crops' = 'Row & Field Crops')) %>%
+  select(code = CODE_BASELINE, color = COLOR, label) %>%
+  arrange(code)
+
+
 mapstack = c(rast('GIS/landscape_rasters/veg_baseline_fall.tif'),
              rast('GIS/scenario_rasters/scenario1_restoration.tif'),
              rast('GIS/scenario_rasters/scenario2_perennialexpand.tif'),
-             rast('GIS/scenario_rasters/scenario3_floodrisk_fall.tif'))
+             rast('GIS/scenario_rasters/scenario3_floodrisk.tif'))
 names(mapstack)[1] = 'baseline'
 
-wkey = read_csv('data/landcover_key_waterbirds.csv', col_types = cols())
+# consolidate land cover classifications for mapping purposes
+mapstack = classify(mapstack,
+                    rcl = matrix(
+                      c(15, 11, #generic orchard (but keep vineyard separate)
+                        22, 21, #wheat lumped with grains)
+                        28, 26, #row lumped with field
+                        71, 70, #combine riparian subclasses
+                        72, 70,
+                        73, 70,
+                        74, 70,
+                        75, 70,
+                        76, 70,
+                        77, 70,
+                        81, 89, #combine perennial and seasonal managed wetlands
+                        82, 89),
+                      byrow = TRUE, ncol = 2))
+freq(mapstack)
 
-key_simple <- wkey %>% select(label, col) %>% distinct() %>%
-  mutate(label = factor(label,
-                        levels = c('water', 'tidal marsh', 'managed wetland',
-                                   'other wetland', 'riparian', 'corn',
-                                   'alfalfa', 'grain', 'field/row', 'rice',
-                                   'orchard/vineyard', 'fallow', 'pasture',
-                                   'grassland', 'forest/shrub', 'barren',
-                                   'urban'))) %>%
-  arrange(label)
+# MANUALLY-------
 
+# consider resampling rasters to prevent the file size from being too enormous
+# (this is still fairly hi-res)
+# scenarios_sampled_hi = mapstack %>% spatSample(size = 5000000, as.raster = TRUE)
 
-# this does take a minute or two and might give error messages - safe to ignore?
-m = DeltaMultipleBenefits::map_scenarios(
-  rast = mapstack,
-  key = wkey %>% select(value = WATERBIRD_CODE, col, label),
-  key_alt = key_simple,
-  rast_opacity = 0.7, leg_opacity = 0.7)
+# change projection and switch to raster package (can't use terra with leaflet)
+scenarios = terra::project(
+  mapstack,
+  y = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext+no_defs",
+  method = 'near') %>%
+  raster::stack()
+
+# color palette
+pal <- leaflet::colorFactor(palette = key_simple$color,
+                            domain = key_simple$code,
+                            alpha = TRUE,
+                            na.color = "transparent")
+
+# base map plus legend
+m <- leaflet::leaflet(scenarios[[1]]) %>%
+  leaflet::addProviderTiles("Stamen.TonerLite") %>%
+  leaflet::addLayersControl(
+    position = "bottomleft",
+    options = leaflet::layersControlOptions(collapsed = F),
+    baseGroups = names(scenarios)) %>%
+  leaflet::addLegend("topright",
+                     labels = key_simple$label,
+                     colors = key_simple$color,
+                     title = 'Land Cover Class',
+                     opacity = 1)
+
+# add scenario rasters
+for (i in c(1:raster::nlayers(scenarios))) {
+  m <- m %>% leaflet::addRasterImage(x = scenarios[[i]],
+                                     group = names(scenarios)[i],
+                                     colors = pal,
+                                     opacity = 0.8,
+                                     project = FALSE)
+}
 
 # add delta boundary
-delta_poly = read_sf('GIS/boundaries/Legal_Delta_Boundary.shp') %>%
-  st_transform(crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 m <- m %>%
   leaflet::addPolygons(data = delta_poly, fill = FALSE, color = 'black',
                        weight = 2, opacity = 1)
 
-## save the interactive map as an html widget
+
+# SAVE-------
+# save the interactive map as an html widget
 # *Note: you may want to change this to "selfcontained = TRUE", which would
 # allow you to email the html file to anyone to view -- the file size just might
 # be super large!
@@ -47,71 +109,19 @@ htmlwidgets::saveWidget(m,
                         selfcontained = FALSE, libdir = 'lib',
                         title = 'Draft scenarios of landcover change in the Delta')
 
-# old:
-# %>%
-#   project(y = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext+no_defs",
-#           method = 'near')
-# names(mapstack)[1] = c('baseline')
+
+# TEST FUNCTION---------
+# # this does take a minute or two and might give error messages - safe to ignore?
+# m = DeltaMultipleBenefits::map_scenarios(
+#   rast = mapstack,
+#   key = wkey %>% select(value = WATERBIRD_CODE, col, label),
+#   key_alt = key_simple,
+#   rast_opacity = 0.7, leg_opacity = 0.7)
 #
-# scenarios = raster::stack(mapstack) #can't use terra for this
-
-# pal <- leaflet::colorFactor(palette = wkey$col, domain = wkey$WATERBIRD_CODE,
-#                             alpha = TRUE, na.color = "transparent")
-#
-
-
-# m <- leaflet::leaflet(scenarios[[1]]) %>%
-#   leaflet::addProviderTiles("Stamen.TonerLite") %>%
-#   leaflet::addLayersControl(
-#     position = "bottomleft",
-#     options = leaflet::layersControlOptions(collapsed = F),
-#     baseGroups = names(scenarios)) %>%
-#   leaflet::addLegend("topright",
-#                      labels = key_simple$label,
-#                      colors = key_simple$col,
-#                      title = 'Land Cover Class',
-#                      opacity = 1)
-#
-# for (i in c(1:raster::nlayers(scenarios))) {
-#   m <- m %>% leaflet::addRasterImage(x = scenarios[[i]],
-#                                      group = names(scenarios)[i], colors = pal,
-#                                      opacity = 0.7,
-#                                      project = FALSE)
-# }
-
-# # consider resampling rasters to prevent the file size from being too enormous (this is
-# # still fairly hi-res)
-# scenarios_sampled_hi = pred_cv %>%
-#   terra::spatSample(size = 5000000, as.raster = TRUE)
-
-# cl <- colors()
-# cl[grep('blue', cl)]
-# scales::show_col(cl[grep('blue', cl)])
-#
-# wkey2 = wkey %>%
-#   mutate(label = factor(label, levels = c('alfalfa', 'corn', 'rice', 'grain',
-#                                           'row/field crop','orchard/vineyard',
-#                                           'pasture', 'grassland', 'water',
-#                                           'wetland', 'managed wetland',
-#                                           'riparian', 'forest/shrub',
-#                                           'fallow', 'barren', 'urban')))
-# scales::show_col(wkey2$col)
-
-
-# # try alternative:
-# m1 = DeltaMultipleBenefits::map_scenarios(
-#   rast = mapstack$baseline,
-#   key = wkey %>% select(value = code, col, label)) %>%
+# # separately add delta boundary
+# delta_poly = read_sf('GIS/boundaries/Legal_Delta_Boundary.shp') %>%
+#   st_transform(crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+# m <- m %>%
 #   leaflet::addPolygons(data = delta_poly, fill = FALSE, color = 'black',
 #                        weight = 2, opacity = 1)
-# m2 = DeltaMultipleBenefits::map_scenarios(
-#   rast = mapstack[[2:4]],
-#   key = wkey %>% select(value = code, col, label)) %>%
-#   leaflet::addPolygons(data = delta_poly, fill = FALSE, color = 'black',
-#                        weight = 2, opacity = 1)
-# testsyncmap = leafsync::sync(m1, m2, ncol = 2)
 #
-# htmlwidgets::saveWidget(testsyncmap,
-#                         'docs/draft_scenarios.html',
-#                         selfcontained = FALSE, libdir = 'lib',
-#                         title = 'Draft scenarios of landcover change in the Delta')
