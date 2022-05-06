d862af18e6921f5290ca3cd9d15d45d9ba93555f
Kristen Dybala <kdybala@gmail.com>
2022-05-06 14:40:08 -0700

    remove total score and retain riparian, nonbreeding waterbird scores

--- R/compile_metrics4_avianconservation.R
+++ R/compile_metrics4_avianconservation.R
@@ -13,14 +13,14 @@ acs_raw <- read_csv('data_orig/Peterson_et_al_2020/Avian_conservation_scores.csv
   pivot_longer(Grassland_landbirds:Breeding_waterbirds,
                names_to = 'TAXA', values_to = 'value')
 
-# add total score (from original effort, including all taxa, rescaled 0-1)
-acs_overall = acs_raw %>% select(LANDCOVER, value = Rescaled) %>% distinct() %>%
-  mutate(TAXA = 'all')
-
-acs_compiled = bind_rows(acs_raw, acs_overall)
+# # add total score (from original effort, including all taxa, rescaled 0-1)
+# acs_overall = acs_raw %>% select(LANDCOVER, value = Rescaled) %>% distinct() %>%
+#   mutate(TAXA = 'all')
+#
+# acs_compiled = bind_rows(acs_raw, acs_overall)
 
 # classify landcovers------
-acs_classify = acs_compiled %>%
+acs_classify = acs_raw %>%
   select(LANDCOVER, TAXA, value) %>%
   mutate(
     CODE_NAME = case_when(
@@ -29,55 +29,89 @@ acs_classify = acs_compiled %>%
       LANDCOVER == 'cotton' ~ 'FIELD', #not very relevant to Delta?
       LANDCOVER == 'corn' ~ 'FIELD_CORN',
       LANDCOVER == 'tomato' ~ 'ROW',
-      LANDCOVER == 'cereal' ~ 'GRAIN',
+      LANDCOVER == 'cereal' ~ 'GRAIN&HAY',
       LANDCOVER == 'pasture' ~ 'PASTURE',
       LANDCOVER == 'alfalfa' ~ 'PASTURE_ALFALFA',
+      LANDCOVER == 'wetland' ~ 'WETLAND_MANAGED',
       TRUE ~ toupper(LANDCOVER))
     )
 
-# filter-------
-# include only breeding shorebirds, waterbirds, and waterfowl (not included in
-# waterbird spatial distribution modeling), plus oak savannah and grassland
-# landbirds
+# fill in missing------
+# treat missing wheat as equivalent to other grain&hay
+# treat woodland/scrub as having value for oak woodland, riparian, and grassland birds
+# treat wetland_other as having same value as grassland
+# assume idle, barren, urban have no value for any?
 
-acs_filter = acs_classify %>%
-  filter(TAXA %in%
-           c('Breeding_shorebirds', 'Breeding_waterbirds', 'Breeding_waterfowl',
-             'Grassland_landbirds', 'Oaksavannah_landbirds'))
+acs_fill = acs_classify %>%
+  bind_rows(
+    acs_classify %>%
+      filter(CODE_NAME == 'GRAIN&HAY') %>%
+      mutate(CODE_NAME = recode(CODE_NAME,
+                                'GRAIN&HAY' = 'GRAIN&HAY_WHEAT')),
+    acs_classify %>%
+      filter(CODE_NAME == 'GRASSLAND') %>%
+      mutate(LANDCOVER = 'woodland', CODE_NAME = 'WOODLAND&SCRUB',
+             value = case_when(TAXA == 'Oaksavannah_landbirds' ~ 3,
+                               TAXA == 'Grassland_landbirds' ~ 1,
+                               TAXA == 'Riparian_landbirds' ~ 2,
+                               TRUE ~ 0)),
+    acs_classify %>%
+      filter(CODE_NAME == 'GRASSLAND') %>%
+      mutate(LANDCOVER = 'wetland_other', CODE_NAME = 'WETLAND_OTHER'),
+    acs_classify %>%
+      filter(CODE_NAME == 'GRASSLAND') %>%
+      mutate(LANDCOVER = 'fallow', CODE_NAME = 'IDLE', value = 0),
+    acs_classify %>%
+      filter(CODE_NAME == 'GRASSLAND') %>%
+      mutate(LANDCOVER = 'barren', CODE_NAME = 'BARREN', value = 0),
+    acs_classify %>%
+      filter(CODE_NAME == 'GRASSLAND') %>%
+      mutate(LANDCOVER = 'urban', CODE_NAME = 'URBAN', value = 0)
+  )
 
 # explore plot
-acs_filter %>%
+acs_fill %>%
   ggplot(aes(value, CODE_NAME)) +
-  geom_col(aes(fill = value), color = 'black') +
+  geom_col(aes(fill = as.factor(value)), color = 'black') +
   facet_wrap(~TAXA)
+
 
 # add combined scores-----
 acs_sum = bind_rows(
-  acs_filter,
-  # all included taxa
-  acs_filter %>%
-    group_by(CODE_NAME, LANDCOVER) %>%
-    summarize(value = sum(value), .groups = 'drop') %>%
-    mutate(TAXA = 'ALL TAXA',
-           #rescale from max possible score (15) to max of 3
-           value = value / (15/3)),
+  acs_fill,
   # breeding waterbirds
-  acs_filter %>%
+  acs_fill %>%
     filter(TAXA %in%
              c('Breeding_shorebirds', 'Breeding_waterbirds',
                'Breeding_waterfowl')) %>%
     group_by(CODE_NAME, LANDCOVER) %>%
     summarize(value = sum(value), .groups = 'drop') %>%
-    mutate(TAXA = 'All breeding waterbirds',
+    mutate(TAXA = 'breeding waterbirds',
            value = value / (9/3)),
   # breeding landbirds
-  acs_filter %>%
-    filter(TAXA %in% c('Grassland_landbirds', 'Oaksavannah_landbirds')) %>%
+  acs_fill %>%
+    filter(TAXA %in% c('Grassland_landbirds', 'Oaksavannah_landbirds', 'Riparian_landbirds')) %>%
     group_by(CODE_NAME, LANDCOVER) %>%
     summarize(value = sum(value), .groups = 'drop') %>%
-    mutate(TAXA = 'All breeding landbirds',
-           value = value / (6/3))
+    mutate(TAXA = 'breeding landbirds',
+           value = value / (9/3)),
+  # wintering waterbirds
+  acs_fill %>%
+    filter(TAXA %in%
+             c('Wintering_shorebirds', 'Wintering_waterbirds',
+               'Wintering_waterfowl')) %>%
+    group_by(CODE_NAME, LANDCOVER) %>%
+    summarize(value = sum(value), .groups = 'drop') %>%
+    mutate(TAXA = 'wintering waterbirds',
+           value = value / (9/3)),
   )
+
+# explore plot
+acs_sum %>%
+  filter(TAXA %in% c('breeding waterbirds', 'breeding landbirds', 'wintering waterbirds')) %>%
+  ggplot(aes(value, CODE_NAME)) +
+  geom_col(aes(fill = value), color = 'black') +
+  facet_wrap(~TAXA, )
 
 # finalize--------
 acs_final = acs_sum %>%
@@ -87,14 +121,5 @@ acs_final = acs_sum %>%
   select(METRIC_CATEGORY, METRIC_SUBTYPE, METRIC = TAXA, CODE_NAME,
          SCORE = value, UNIT)
 
-write_csv(acs_final, 'data/multiplebenefits/avian_conservation_score.csv')
+write_csv(acs_final, 'data/avian_conservation_score.csv')
 
-# explore plot
-acs_final %>%
-  mutate(CODE_NAME = factor(CODE_NAME,
-                            levels = acs_final %>%
-                              filter(METRIC == 'ALL TAXA') %>%
-                              arrange(SCORE) %>% pull(CODE_NAME))) %>%
-  ggplot(aes(SCORE, CODE_NAME)) +
-  geom_col(aes(fill = SCORE), color = 'black') +
-  facet_wrap(~METRIC)
