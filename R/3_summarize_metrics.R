bbef68fd0b2ef799d03f36c811e1d14a2b60e9c1
Kristen Dybala <kdybala@gmail.com>
2022-12-09 16:16:29 -0800

    exclude salinity from metrics; add units metadata

--- R/3_summarize_metrics.R
+++ R/3_summarize_metrics.R
@@ -1,4 +1,7 @@
-# PLOTS OF INPUT DATA
+# README
+# compile separate metrics into a final set for use in scenario analyses and R
+# package; plot each set of metrics and example metrics for presentation
+# purposes
 
 # PACKAGES & FUNCTIONS
 source('R/0_packages.R')
@@ -16,6 +19,7 @@ label.order = key %>% select(CODE_NAME, LABEL) %>% distinct() %>%
 label.order = label.order[c(1:9, 11:13, 10, 14:22)]
 
 # COMPILE ALL METRICS----------
+
 waterquality = read_csv('data/pesticide_exposure.csv', col_types = cols()) %>%
   filter(METRIC %in%
            c('Risk to Aquatic Organisms', 'Critical Pesticides',
@@ -27,52 +31,69 @@ economy = bind_rows(
   #change jobs metrics back to jobs/ha for easier landscape calculations
   mutate(across(c(SCORE_MEAN, SCORE_SE, SCORE_MIN, SCORE_MAX),
                 ~if_else(METRIC == 'Agricultural Jobs', .x/100, .x)),
-         UNIT = recode(UNIT, 'number of employees per 100ha' = 'employees per ha'))
+         UNIT = recode(UNIT,
+                       'number of employees per 100ha' = 'employees per ha'))
 
-ccs = read_csv('data/climate_change_resilience.csv', col_types = cols())
+ccs = read_csv('data/climate_change_resilience.csv', col_types = cols()) %>%
+  filter(METRIC %in% c('Heat', 'Drought', 'Flood'))
 
-# acs = read_csv('data/avian_conservation_score.csv', col_types = cols())
 
 metrics = bind_rows(economy, waterquality, ccs) %>%
   left_join(key %>% select(LABEL, CODE_NAME), by = 'CODE_NAME') %>%
-  mutate(CODE_NAME = factor(CODE_NAME,
-                            levels = key$CODE_NAME %>% na.omit()),
-         METRIC_CATEGORY = factor(METRIC_CATEGORY,
-                                  levels = c('economy', 'water quality',
-                                             'climate', 'biodiversity')),
-         METRIC_CATEGORY = recode(METRIC_CATEGORY,
-                                  economy = 'Agricultural Livelihoods',
-                                  `water quality` = 'Water Quality',
-                                  climate = 'Climate Change Resilience',
-                                  biodiversity = 'Biodiversity Support'),
-         METRIC = gsub('_', ': ', METRIC)) %>%
-  mutate_at(vars(METRIC_SUBTYPE:METRIC), factor) %>%
-  arrange(METRIC_CATEGORY, METRIC_SUBTYPE, METRIC, CODE_NAME)
-write_csv(metrics, 'output/metrics_final.csv')
+  mutate(
+    CODE_NAME = factor(
+      CODE_NAME,
+      levels = key$CODE_NAME %>% na.omit()),
+    METRIC_CATEGORY = recode(
+      METRIC_CATEGORY,
+      economy = 'Agricultural Livelihoods',
+      `water quality` = 'Water Quality',
+      climate = 'Climate Change Resilience',
+      biodiversity = 'Biodiversity Support'),
+    UNIT = recode(
+      UNIT,
+      'employees per ha' = 'FTE/ha/yr',
+      'annual dollars per employee (thousands)' = 'USD/FTE, thousands',
+      'USD per ha per year (millions)' = 'USD/ha/yr, millions',
+      'kg per ha' = 'kg/ha/yr',
+      'ranking' = 'qualitative score, 1-10')) %>%
+  arrange(METRIC_CATEGORY, METRIC, CODE_NAME) %>%
+  select(METRIC_CATEGORY, METRIC, UNIT, CODE_NAME, LABEL, SCORE_MEAN, SCORE_SE)
+write_csv(metrics, 'output/metrics.csv')
+
+metrics_meta = metrics %>% select(METRIC_CATEGORY, METRIC, UNIT) %>%
+  distinct()
+write_csv(metrics_meta, 'output/metrics_metadata.csv')
 
 # SUMMARY TABLE---------
 # for supplemental info in manuscript
-metrics_table = metrics %>%
+metrics = read_csv('output/metrics.csv')
+
+metrics_format = metrics %>%
   filter(LABEL %in% label.order) %>%
   mutate(LABEL = factor(LABEL, levels = label.order)) %>%
   arrange(LABEL) %>%
-  # for the purposes of this table, convert to jobs/100ha
+  # for the readability of this table, convert Ag Jobs units
   mutate(across(c(SCORE_MEAN, SCORE_SE),
                 ~if_else(METRIC == 'Agricultural Jobs',
-                         .x*100, .x))) %>%
-  mutate(SCORE_MEAN = paste0("'",round(SCORE_MEAN, digits = 2) %>% format(nsmall = 2)),
-         SCORE_SE = paste0("'(", round(SCORE_SE, digits = 2) %>% format(nsmall = 2), ')')
-         # SCORE = paste0(SCORE_MEAN, ' (', SCORE_SE, ')'),
-         # SCORE = gsub(' \\(  NA\\)', '', SCORE)
-  ) %>%
+                         .x*100, .x)))
+
+metrics_table = metrics_format %>%
+  mutate(SCORE_MEAN = paste0("'", round(SCORE_MEAN, digits = 2) %>%
+                               format(nsmall = 2)),
+         SCORE_SE = paste0("'(", round(SCORE_SE, digits = 2) %>%
+                             format(nsmall = 2), ')')) %>%
   select(LABEL, METRIC_CATEGORY, METRIC, SCORE_MEAN, SCORE_SE) %>%
-  pivot_wider(names_from = c(METRIC_CATEGORY, METRIC), values_from = c(SCORE_MEAN, SCORE_SE)) %>%
+  pivot_wider(names_from = c(METRIC_CATEGORY, METRIC),
+              values_from = c(SCORE_MEAN, SCORE_SE)) %>%
   select(LABEL,
          contains('Jobs'), contains('Wages'), contains('Gross'),
          contains('Critical'), contains('Groundwater'), contains('Aquatic'),
          contains('Drought'), contains('Flood'), contains('Heat'),
          contains('Salinity'))
 write_csv(metrics_table, 'output/TABLE_metrics_summary.csv')
+
+
 
 # PLOTS BY METRIC--------
 library(showtext)
@@ -82,7 +103,8 @@ showtext_opts(dpi = 300)
 
 ## ag livelihoods------
 metrics %>%
-  filter(METRIC_CATEGORY=='Agricultural Livelihoods' & LABEL %in% label.order) %>%
+  filter(METRIC_CATEGORY=='Agricultural Livelihoods' &
+           LABEL %in% label.order) %>%
   mutate(LABEL = factor(LABEL, levels = rev(label.order))) %>%
   select(LABEL, CODE_NAME, METRIC, SCORE_MEAN, SCORE_SE) %>%
   # for plotting purposes, convert to ag jobs/100 ha
@@ -93,16 +115,19 @@ metrics %>%
   geom_errorbar(aes(xmin = SCORE_MEAN - SCORE_SE,
                     xmax = SCORE_MEAN + SCORE_SE),
                 width = 0.5) +
-  facet_wrap(~METRIC, ncol = 3, scales = 'free_x',
-             labeller = labeller(
-               METRIC = c(`Agricultural Jobs` = 'Agricultural Jobs\n(FTE / 100 ha)',
-                          `Annual Wages` = 'Annual Wages\n(USD, thousands)',
-                          `Gross Production Value` = 'Gross Crop Production Value\n(USD / ha, millions)'))) +
+  facet_wrap(
+    ~METRIC, ncol = 3, scales = 'free_x',
+    labeller = labeller(
+      METRIC = c(
+        `Agricultural Jobs` = 'Agricultural Jobs\n(FTE / 100 ha)',
+        `Annual Wages` = 'Annual Wages\n(USD, thousands)',
+        `Gross Production Value` = 'Gross Crop Production Value\n(USD / ha, millions)'))) +
   labs(x = NULL, y = NULL) +
   theme_bw() +
   theme(axis.text = element_text(family = 'sourcesans', size = 10),
         axis.title = element_text(family = 'sourcesans', size = 12),
-        strip.text = element_text(family = 'sourcesans', size = 10, hjust = 0, vjust = 1),
+        strip.text = element_text(family = 'sourcesans', size = 10,
+                                  hjust = 0, vjust = 1),
         strip.background = element_blank(),
         legend.position = 'none')
 ggsave('fig/metrics_aglivelihoods.png', height = 4, width = 8)
@@ -129,15 +154,18 @@ ggsave('fig/metrics_waterquality.png', height = 4, width = 8)
 
 ## climate change resilience---------
 metrics %>%
-  filter(METRIC_CATEGORY=='Climate Change Resilience' & LABEL %in% label.order) %>%
-  mutate(LABEL = factor(LABEL, levels = rev(label.order)),
-         METRIC = factor(METRIC, levels = c('Drought', 'Flood', 'Heat', 'Salinity', 'Overall'))) %>%
+  filter(METRIC_CATEGORY=='Climate Change Resilience' &
+           LABEL %in% label.order) %>%
+  mutate(
+    LABEL = factor(LABEL, levels = rev(label.order)),
+    METRIC = factor(METRIC,
+                    levels = c('Drought', 'Flood', 'Heat'))) %>%
   select(LABEL, CODE_NAME, METRIC, SCORE_MEAN, SCORE_SE) %>%
   ggplot(aes(SCORE_MEAN, LABEL)) + geom_col(fill = 'seagreen') +
   geom_errorbar(aes(xmin = SCORE_MEAN - SCORE_SE,
                     xmax = SCORE_MEAN + SCORE_SE),
                 width = 0.5) +
-  facet_wrap(~METRIC, ncol = 5, scale = 'free_x') +
+  facet_wrap(~METRIC, ncol = 3, scale = 'free_x') +
   labs(x = 'Climate change resilience score', y = NULL) +
   theme_bw() +
   theme(axis.text.x = element_text(family = 'sourcesans', size = 9),
@@ -147,3 +175,90 @@ metrics %>%
         strip.background = element_blank(),
         legend.position = 'none')
 ggsave('fig/metrics_climate.png', height = 4, width = 8)
+
+## vineyard example--------
+# just for presentation purposes -- one of each metric for vineyards
+metrics_example = read_csv('output/metrics.csv') %>%
+  filter(LABEL == 'Vineyard') %>%
+  mutate(
+    # rescale to match units from netchange
+    across(c(SCORE_MEAN, SCORE_SE),
+           ~case_when(
+             METRIC == 'Agricultural Jobs' ~ .*1000, #per ha to per 1000 ha
+             TRUE ~ .)),
+    METRIC = factor(
+      METRIC,
+      levels = c("Agricultural Jobs", "Annual Wages", "Gross Production Value",
+                 "Critical Pesticides", "Groundwater Contaminant",
+                 "Risk to Aquatic Organisms", "Drought", "Flood", "Heat") %>%
+        rev()),
+    # add units to ag livelihood metric labels
+    METRIC = recode(
+      METRIC,
+      'Agricultural Jobs' = 'Agricultural Jobs\n(FTE per 1000 ha)',
+      'Annual Wages' = 'Annual Wages\n(USD, thousands)',
+      'Gross Production Value' = 'Gross Production Value\n(USD, billions)',
+      'Critical Pesticides' = 'Critical\nPesticides',
+      'Groundwater Contaminant' = 'Groundwater\nContaminants',
+      'Risk to Aquatic Organisms' = 'Risk to Aquatic\nOrganisms'))
+
+# ag livelihoods
+a = metrics_example %>%
+  filter(METRIC_CATEGORY == 'Agricultural Livelihoods') %>%
+  ggplot(aes(SCORE_MEAN, METRIC)) +
+  geom_col(fill = pointblue.palette[4], na.rm = TRUE) +
+  geom_errorbar(aes(xmin = SCORE_MEAN - SCORE_SE, xmax = SCORE_MEAN + SCORE_SE),
+                width = 0.5) +
+  labs(x = '', y = NULL, title = 'Agricultural Livelihoods') +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, 42)) +
+  theme_bw() +
+  theme(axis.text = element_text(family = 'sourcesans', size = 16),
+        axis.title = element_text(family = 'sourcesans', size = 16),
+        strip.text = element_text(family = 'sourcesans', size = 18, hjust = 0),
+        plot.title = element_text(family = 'sourcesans', size = 18),
+        strip.background = element_blank(),
+        panel.grid = element_blank(),
+        legend.position = 'none')
+
+# pesticides
+b = metrics_example %>%
+  filter(METRIC_CATEGORY == 'Water Quality') %>%
+  ggplot(aes(SCORE_MEAN, METRIC)) +
+  geom_col(fill = pointblue.palette[3]) +
+  geom_errorbar(aes(xmin = SCORE_MEAN - SCORE_SE, xmax = SCORE_MEAN + SCORE_SE),
+                width = 0.5) +
+  labs(x = 'Pesticide application rate (kg/ha/yr)', y = NULL,
+       title = 'Water Quality') +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, 3)) +
+  theme_bw() +
+  theme(axis.text = element_text(family = 'sourcesans', size = 16),
+        axis.title = element_text(family = 'sourcesans', size = 16),
+        strip.text = element_text(family = 'sourcesans', size = 18, hjust = 0),
+        strip.background = element_blank(),
+        plot.title = element_text(family = 'sourcesans', size = 18),
+        panel.grid = element_blank(),
+        legend.position = 'none')
+
+# climate change resilience
+c = metrics_example %>%
+  filter(METRIC_CATEGORY == 'Climate Change Resilience') %>%
+  ggplot(aes(SCORE_MEAN, METRIC)) +
+  geom_col(fill = 'seagreen', na.rm = TRUE) +
+  geom_errorbar(aes(xmin = SCORE_MEAN - SCORE_SE, xmax = SCORE_MEAN + SCORE_SE), width = 0.5) +
+  labs(x = 'Mean score (1-10 scale)', y = NULL, title = 'Climate change resilience') +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, 10), breaks = seq(0, 10, 2)) +
+  theme_bw() +
+  theme(axis.text = element_text(family = 'sourcesans', size = 16),
+        axis.title = element_text(family = 'sourcesans', size = 16),
+        strip.text = element_text(family = 'sourcesans', size = 18, hjust = 0),
+        strip.background = element_blank(),
+        plot.title = element_text(family = 'sourcesans', size = 18),
+        panel.grid = element_blank(),
+        legend.position = 'none')
+
+showtext_auto()
+showtext_opts(dpi = 300) #default for ggsave
+a + b + c + plot_layout(nrow = 2, byrow = FALSE)
+ggsave('fig/presentation_metrics_overview.png',
+       height = 7.5, width = 10, units = 'in')
+showtext_auto(FALSE)
